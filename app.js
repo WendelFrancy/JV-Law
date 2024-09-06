3d8d41cdc684a313c5ba72b3c339e22cd8466ea7
WendelF <wendelfas@hotmail.com>
2024-09-06 16:36:12 -0300

    Animacao Home-Title e BTN Topo

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const observer = new IntersectionObserver((entries) => {
         console.log(entry)
         if(entry.isIntersecting) {
             entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
         }
     })
 })
