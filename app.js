bc34c444c5b159c6b8c4cab23ed235afea546a34
WendelF <wendelfas@hotmail.com>
2024-09-16 09:47:13 -0300

    PF5.1

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ btnsubmit.addEventListener('click', (e) => {
 })
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
+        entries.forEach((entry) => {
+        console.log(entries)
+        if(entry.intersectionRatio > 0) {
+            entry.target.classList.add('show')
+            observer.takeRecords(entry.target);
+        } 
     })
 })
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+console.log(hiddenElements)
