10e9461fba966dd9fe64d656b3e8284f0ec71c54
WendelF <wendelfas@hotmail.com>
2024-10-16 16:36:04 -0300

    Responsive 4.0

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ btnsubmit.addEventListener('click', (e) => {
     document.querySelector('.txtarea').value = "";
 })
 
-const observer = new IntersectionObserver((entries) => {
+function textslide (){
+    const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
         console.log(entries)
         if(entry.intersectionRatio > 0) {
@@ -22,5 +23,66 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+}
 
-console.log(hiddenElements)
+textslide();
+
+const cardsContainer = document.querySelector('.specialistscards');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+cardsContainer.addEventListener('mousedown', (e) => {
+    isDown = true;
+    startX = e.pageX - cardsContainer.offsetLeft;
+    scrollLeft = cardsContainer.scrollLeft;
+});
+
+cardsContainer.addEventListener('mouseleave', () => {
+    isDown = false;
+});
+
+cardsContainer.addEventListener('mouseup', () => {
+    isDown = false;
+});
+
+cardsContainer.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - cardsContainer.offsetLeft;
+    const walk = (x - startX) * 2; // Ajuste a sensibilidade aqui
+    cardsContainer.scrollLeft = scrollLeft - walk;
+
+    // Lógica para reposicionar os cartões
+    if (cardsContainer.scrollLeft < 208) {
+        cardsContainer.scrollLeft += 600; // Ajuste conforme necessário
+    } else if (cardsContainer.scrollLeft > 832) { // Ajuste para o limite máximo
+        cardsContainer.scrollLeft -= 600;
+    }
+});
+
+// Para toque em telas sensíveis
+cardsContainer.addEventListener('touchstart', (e) => {
+    isDown = true;
+    startX = e.touches[0].pageX - cardsContainer.offsetLeft;
+    scrollLeft = cardsContainer.scrollLeft;
+});
+
+cardsContainer.addEventListener('touchend', () => {
+    isDown = false;
+});
+
+cardsContainer.addEventListener('touchmove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.touches[0].pageX - cardsContainer.offsetLeft;
+    const walk = (x - startX) * 2; // Ajuste a sensibilidade aqui
+    cardsContainer.scrollLeft = scrollLeft - walk;
+    
+    // Lógica para reposicionar os cartões
+    if (cardsContainer.scrollLeft < 200) {
+        cardsContainer.scrollLeft += 400; // Ajuste conforme necessário
+    } else if (cardsContainer.scrollLeft > 800) { // Ajuste para o limite máximo
+        cardsContainer.scrollLeft -= 600;
+    }
+});
